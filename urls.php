69cadf66d501351cf0ea825ea9f1a8c8bd272c5c
Lernoy <sachakaverin0@gmail.com>
2023-08-15 17:14:19 +0300

    Sync files

--- urls.php
+++ urls.php
@@ -1,30 +1,32 @@
-<?php 
-include($_SERVER["DOCUMENT_ROOT"]."/templates/sql.php");
+<?php
+include($_SERVER["DOCUMENT_ROOT"] . "/templates/sql.php");
 
 
 
 $sql = "SHOW TABLES";
 $result = $conn->query($sql);
-
+$section = explode("/", $_SERVER['REQUEST_URI'])[1];
+$arrPageCodes = [];
 if ($result->num_rows > 0) {
-  while($row = $result->fetch_assoc()) {
-    if (str_contains($_SERVER["REQUEST_URI"], $row["Tables_in_j6531130_cms"]) !== false) {
-        if ($_SERVER["REQUEST_URI"] != "/".$row["Tables_in_j6531130_cms"]."/")
-            include($_SERVER['DOCUMENT_ROOT'] . "/".$row["Tables_in_j6531130_cms"]."/page.php");
-        else
-            include($_SERVER['DOCUMENT_ROOT'] . "/".$row["Tables_in_j6531130_cms"]."/index.php");
-    }
+  while ($row = $result->fetch_assoc()) {
+    $arrPageCodes[] = $row["Tables_in_j6531130_cms"];
   }
 }
-if($_SERVER["REQUEST_URI"]=="/") require($_SERVER["DOCUMENT_ROOT"]."/index.php");
 
 
-function pageCheck($pageCode){
-    if (str_contains($_SERVER["REQUEST_URI"], $pageCode) !== false) {
-        if ($_SERVER["REQUEST_URI"] != "/".$pageCode."/")
-            include($_SERVER['DOCUMENT_ROOT'] . "/".$pageCode."/page.php");
-        else
-            include($_SERVER['DOCUMENT_ROOT'] . "/".$pageCode."/index.php");
-    }
-}te
-?>
+if ($_SERVER["REQUEST_URI"] == "/") require($_SERVER["DOCUMENT_ROOT"] . "/index.php");
+
+if (in_array($section, $arrPageCodes) && file_exists($_SERVER['DOCUMENT_ROOT'] . "/" . $section . "/")) {
+  if ($_SERVER["REQUEST_URI"] != "/" . $section . "/")
+    include($_SERVER['DOCUMENT_ROOT'] . "/" . $section . "/page.php");
+  else
+    include($_SERVER['DOCUMENT_ROOT'] . "/" . $section . "/index.php");
+} 
+else if(explode(".", $_SERVER["REQUEST_URI"])[1] == "php"){
+  include($_SERVER['DOCUMENT_ROOT'] . $_SERVER["REQUEST_URI"]);
+}
+else {
+  http_response_code(404);
+  include('404.php'); // provide your own HTML for the error page
+  die();
+}
