0473fa213cd4264f47279cfa729491567b8b96e5
Lernoy <70978244+Lernoy@users.noreply.github.com>
2023-08-17 10:41:45 +0300

    Update sql.php

--- templates/sql.php
+++ templates/sql.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
-$username = "j6531130_cms";
-$dbname = "j6531130_cms";
-$password = "Qazwsxed35";
+$username = "$username";
+$dbname = "$dbname";
+$password = "$password";
 $conn = new mysqli($servername, $username, $password, $dbname);
 require($_SERVER["DOCUMENT_ROOT"]."/templates/helpers/sections_id.php");
